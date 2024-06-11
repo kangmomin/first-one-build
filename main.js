7c4ebaf8fbb45e557e14acf00d016e76272ede30
koldin108902 <alex108902@naver.com>
2024-06-11 19:11:38 +0900

    list view update

---
+++ main.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const path_1 = require("path");
+const express_1 = require("express");
+const hbs = require("hbs");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const process = require("process");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use(cookieParser());
+    app.use(session({
+        secret: 'your-secret-key',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 86400000 }
+    }));
+    app.useStaticAssets((0, path_1.join)(__dirname, '..', 'public'));
+    app.setBaseViewsDir((0, path_1.join)(__dirname, '..', 'views'));
+    app.setViewEngine("hbs");
+    app.use((0, express_1.json)({ limit: '50mb' }));
+    hbs.registerHelper("multiple", (index, value) => {
+        return (value / index * 200).toFixed(1).toLocaleString();
+    });
+    hbs.registerHelper("locale", (index) => {
+        return index.toLocaleString();
+    });
+    hbs.registerHelper("len", (index) => {
+        let sum = 0;
+        index.forEach(val => {
+            sum += val.quantity;
+        });
+        return sum;
+    });
+    await app.listen(process.env.PORT);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
--- public/assets/js/custom.js
+++
@@ -1,6 +0,0 @@
-const isCostInclude = document.getElementById("isCostInclude");
-isCostInclude.addEventListener("click", e => {
-    for (let e of document.getElementsByClassName("isCostInclude")) {
-        e.classList.toggle("d-none")
-    }
-})
